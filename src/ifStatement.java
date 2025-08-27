b2a5f67eaf92d3c99d029262bdaf151d89966b5b
Mohammad E. Karim <ekramulkarim@yahoo.com>
2025-08-27 12:01:16 -0400

    Updated 06/26/26

--- src/ifStatement.java
+++ src/ifStatement.java
@@ -1,32 +1,12 @@
-void main() {
-    int age = 12;
-
-    if (age >= 75) {
-        IO.println("You are too old.");
-    } else if (age >= 18) {
-        IO.println("You are an adult.");
-    } else {
-        IO.println("You are not an adult.");
-import java.util.Scanner;
-
-class Main {
-    static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter your name: ");
-        String name = sc.nextLine();
-
-        System.out.print("Enter your age: ");
-        int age = sc.nextInt();
-
-        if (age >= 75) {
-            System.out.println("You are too old.");
-        } else if (age >= 18) {
+public class ifStatement {
+    public static void main(String[] args) {
+        int age= 12;
+        if (age >= 18) {
             System.out.println("You are an adult.");
+        } else if (age>=75) {
+            System.out.println("You are too old.");
         } else {
             System.out.println("You are not an adult.");
         }
-
-        sc.close();
     }
 }
